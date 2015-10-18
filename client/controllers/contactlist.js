53b60630e48cbbb44348d752eaa44c1fc7c9fac0
petnwil <peter.n.wilhelmsen@gmail.com>
2015-10-18 12:12:38 +1000

    updated details,contact and main

--- client/controllers/contactlist.js
+++ client/controllers/contactlist.js
@@ -9,26 +9,50 @@ Router.route("/", function(){
 
 Router.route('test'); //added route to 'test'. Have to add route for every template I guess
 Router.route('detailContact');
+Router.route('newContact');
 
 
 
 
   Template.contactlist.helpers({
     contacts:function(){
-      return [{name:"Peter Wilhelmsen"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"},{name:"Haakon Winther"}];
+      return contacts.find();
     }
   });
 
 
   Template.contactlist.events({
     'click #addContact':function(event,template){
-      Router.go('/test');
+      Router.go('/newContact');
     },
 
     'click #viewContact':function(event,template){
       /*Need function which find this contacts data and then route it to
       detailContact. Do Router.go() have to be inside this function?? */
 
-      Router.go('/detailContact');
+
+      getInfoContact(this._id);
+      //console.log(contacts.find({_id:id}).fetch());
+
     },
 });
+
+
+
+function getInfoContact(id){
+  //console.log(id);
+  var obj = contacts.find({_id:id}).fetch();
+  console.log(obj);
+  Session.set("firstName",obj[0].firstname);
+  console.log(Session.get("firstName"));
+  Session.set("lastName",obj[0].lastname);
+  console.log(Session.get("lastName"));
+  Session.set("phoneNr",obj[0].number);
+  console.log(Session.get("phoneNr"));
+  Session.set("gender",obj[0].gender);
+  console.log(Session.get("gender"));
+  Session.set("id",obj[0]._id);
+  //set name, etc in detailContact with session variables
+
+  Router.go('/detailContact');
+}
--- client/controllers/detailContact.js
+++ client/controllers/detailContact.js
@@ -1,5 +1,38 @@
+Template.detailContact.helpers({
+  firstName:function(){
+    return Session.get("firstName");
+  },
+  lastName:function(){
+    return Session.get("lastName");
+  },
+  phoneNumber:function(){
+    return Session.get("phoneNumber");
+  },
+  gender:function(){
+    return Session.get("gender");
+  },
+  email:function(){
+    return Session.get("email");
+  },
+  lat:function(){
+    return Session.get("latitude");
+  },
+  long:function(){
+    return Session.get("longitude");
+  },
+  id:function(){
+    return Session.get("id");
+  }
+});
+
 Template.detailContact.events({
   'click #backToContactList':function(event,template){
     Router.go('/');
   },
+
+  'click #deleteButton':function(event,template){
+    console.log(Session.get("phoneNumber"));
+    contacts.remove({_id:Session.get("id")});
+    Router.go('/');
+  }
 });
